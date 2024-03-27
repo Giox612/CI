bc404f729bd661431c91405202d09574adbba3f8
GRODRIGUE002 <grodrigue002@vplincsci053.ad.csbsju.edu>
2024-03-26 20:53:47 -0500

    fixed tests

--- test/cs230/ci/AllTests.java
+++ test/cs230/ci/AllTests.java
@@ -2,13 +2,11 @@ package cs230.ci;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
 import cs230.ci.entities.AssignmentTest;
 import cs230.ci.entities.UserTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({AssignmentTest.class, UserTest.class})
+@Suite.SuiteClasses({AssignmentTest.class, UserTest.class})
 public class AllTests {
 
 }
